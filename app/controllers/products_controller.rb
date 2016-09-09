95d54a60e441a16600416d5248184e1da31accd7
Matteo Arosti <matteo.arosti@gmail.com>
2016-09-10 01:19:06 +0200

    product_list

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,10 +5,23 @@ class ProductsController < ApplicationController
   end
 
   
+  def get_rows
+    items = Product.all
+    render json: {:success => true, :items => items.as_json()}
+  end  
+  
+  
   def get_rows_mc
     items = ProductMc.all
     render json: {:success => true, :items => items.as_json()}
   end
   
+  
+  def product_new 
+    render partial: "product_form", locals: {item: Product.new}
+  end
+    
+  def product_list
+  end
   
 end
